e30d4d8eb3e50b92937d38885261ddc50058b8f6
jeanmarcj <jmjbup@gmail.com>
2021-09-21 15:42:17 +0200

    Feat. Import boostrap and boostrap vue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+//Personal scss for this app
+import "./app.scss";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
