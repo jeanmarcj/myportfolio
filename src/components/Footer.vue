d10d7deafed6c4e43ac46cb6a763cac1f7d05afc
jeanmarcj <jmjbup@gmail.com>
2021-09-30 15:48:09 +0200

    Feat. Footer is now responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="footer-wrapper">
     <hr />
-    <footer class="py-5 text-right">
-      <ul class="mt-3">
-        <li class="mr-3">Portfolio développé en HTML, SASS et JavaScript</li>
-        <li class="mr-3">
+    <footer class="py-5">
+      <ul class="">
+        <li class="">Portfolio développé en HTML, SASS et JavaScript</li>
+        <li class="">
           Framework
           <a href="https://fr.vuejs.org/index.html" target="_blank">Vue.JS</a>
         </li>
-        <li class="mr-3">
+        <li class="">
           <a href="https://github.com/jeanmarcj/myportfolio" target="_blank">
             <b-icon-github scale="1.2"></b-icon-github>
           </a>
@@ -30,10 +30,25 @@ export default {
   margin: 0 auto;
   hr {
     margin-top: 3rem;
+    background-color: var(--text-primary-color);
+  }
+  footer {
+    ul {
+      margin: 0;
+      padding: 0;
+    }
   }
 }
 li {
   list-style: none;
   font-size: 0.8rem;
+  color: var(--text-primary-color);
+}
+
+@media (min-width: 768px) {
+  li {
+    text-align: right;
+    font-size: 1rem;
+  }
 }
 </style>
