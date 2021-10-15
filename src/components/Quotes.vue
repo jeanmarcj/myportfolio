75fbf0a9c94418678387dd399462b43d86f552f5
jeanmarcj <jmjbup@gmail.com>
2021-10-15 08:38:18 +0200

    Fix. Comments updates

--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -17,7 +17,7 @@
       >
         <p class="lead">
           <i>
-            Projet responsive. Sémantique html OK. Validation w3c. Présentation
+            Projet responsif. Sémantique html OK. Validation w3c. Présentation
             fluide et structurée. Tous les choix ont été justifié. Chaque point
             du brief a été clairement expliqué. Bonne compréhension de
             l'intégration d'une maquette. Bonne compréhension de Git.
@@ -45,9 +45,9 @@
       >
         <p class="lead">
           <i>
-            L'aspect visuel corresponds à la maquette sur écran mobile. Le
-            projet est responsive sur mobile, tablette et desktop. Très bon
-            travail, l'étudiant a le souci du détail. Utilisation de BEM et de
+            L'aspect visuel correspond à la maquette sur écran mobile. Le projet
+            est responsif sur mobile, tablette et desktop. Très bon travail,
+            l'étudiant a le souci du détail. Utilisation de BEM et de
             l'architecture 7-1 (SASS). Très bonnes explications. Très bonne
             présentation, les explications sont claires, les réponses aux
             questions montrent que l'étudiant a maîtrisé les notions abordées
@@ -76,7 +76,7 @@
       >
         <p class="lead">
           <i>
-            On retrouve dans le rapport d'analse SEO, des recommandations liées
+            On retrouve dans le rapport d'analyse SEO, des recommandations liées
             à l'accessibilité. Pour chaque problème identifié on retrouve la
             bonne pratique à adopter, l'explication du problème et la référence.
             Le code est correctement indenté et passe la validation W3C. Le
@@ -107,8 +107,8 @@
       >
         <p class="lead">
           <i>
-            Points forts : code JavaScript modulaire. Design travaillé et
-            réalisé. Travail de qualité professionnel. Très bonne présentation
+            Points forts : code JavaScript modulaire, design travaillé et
+            réalisé, travail de qualité professionnelle. Très bonne présentation
             orale : claire, structurée et fluide. Très bonne implication dans la
             mission. Bravo pour cet excellent travail.
           </i>
@@ -162,11 +162,11 @@
       >
         <p class="lead">
           <i>
-            L'application web contient les logos et la gamme de couleur fournis
+            L'application web contient les logos et la gamme de couleurs fournis
             par le client. La page est conforme aux normes WCAG. L'utilisateur
             peut faire des requêtes de données depuis SQL. Points forts :
             l'ajout des "SLUGs" est une très bonne idée. Bonne présentation, tu
-            maîtrise bien ton code. Bon courage pour ta future vie
+            maîtrises bien ton code. Bon courage pour ta future vie
             professionnelle.
           </i>
         </p>
